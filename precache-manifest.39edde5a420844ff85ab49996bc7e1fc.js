d8ff56e15e3a442d7d0bc15a1b3d6eba4241e22e
Walid Djemmaa <walid.djemmaa@gmail.com>
2020-05-21 01:46:53 +0200

    Updates

---
+++ precache-manifest.39edde5a420844ff85ab49996bc7e1fc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72a974e34de6083237346066b25bde7e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "35c3a4f849767dd5fac1",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "28fc905aba0d7b556c2c",
+    "url": "/static/js/2.a6800634.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a6800634.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35c3a4f849767dd5fac1",
+    "url": "/static/js/main.59842b91.chunk.js"
+  },
+  {
+    "revision": "5284eec07972ace79c95",
+    "url": "/static/js/runtime-main.ceaf4b5f.js"
+  },
+  {
+    "revision": "976b4021f4cd4c88c9d1db09a38739d2",
+    "url": "/static/media/profile.976b4021.png"
+  }
+]);
